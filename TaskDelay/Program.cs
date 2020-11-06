e80db45f5268ff42c35e8bff18494df725f99ef7
Dennis Renz <dennis@r-renz.de>
2020-11-06 11:42:16 +0100

    Initial commit

---
+++ TaskDelay/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDelay
+{
+    class Program
+    {
+        static Task WithoutAwait()
+        {
+            Console.WriteLine("Not waiting");
+            return Task.Delay(1000);
+        }
+
+        static async Task WithAwait()
+        {
+            Console.WriteLine("Waiting");
+            await Task.Delay(1000);
+        }
+
+        static void Main(string[] args)
+        {
+            var withoutAwaitTask = new Task(() =>
+            {
+                for (int i = 0; i < 10; ++i)
+                    WithoutAwait();
+            });
+
+            withoutAwaitTask.RunSynchronously();
+
+            var withAwaitTask = new Task(async () =>
+            {
+                for (int i = 0; i < 10; ++i)
+                    await WithAwait();
+            });
+
+            withAwaitTask.RunSynchronously();
+
+            Thread.Sleep(10000);
+        }
+    }
+}
